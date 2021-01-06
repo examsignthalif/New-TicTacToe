24b05cedb6d13132f6a995bdebd4a14702c5c45f
thalif <thalif12@gmail.com>
2021-01-06 23:23:16 +0530

    Ready to Play the Game

--- NewTicTacToe/NewTicTacToe/Program.cs
+++ NewTicTacToe/NewTicTacToe/Program.cs
@@ -16,6 +16,7 @@ namespace NewTicTacToe
         {
             LoadBoard();
             GameStart();
+            Console.ReadKey();
         }
 
         static public void LoadBoard()
@@ -72,97 +73,154 @@ namespace NewTicTacToe
                     i -= Player1Input();
                 else if (Player2)
                     i -= Player2Input();
+
+                if (i >= 4)
+                    if (ValidationProcess(PlayBoard))
+                        break;
             }
         }
 
-        static public void ValidationProcess()
+        static public bool ValidationProcess(string[] arr)
         {
+            if (LeftVerticalCheck(arr))
+                return true;
 
+            if (RightVerticalCheck(arr))
+                return true;
+
+            if (MiddleVerticalCheck(arr))
+                return true;
+
+            if (TopHorizontalCheck(arr))
+                return true;
+            if (MiddleHorizontalCheck(arr))
+                return true;
+            if (BottomHorizontalCheck(arr))
+                return true;
+
+            if (LeftToRightDiagnol(arr))
+                return true;
+
+            if (RightToLeftDiagnol(arr))
+                return true;
+            return false;
         }
 
-        static public void LeftVerticalCheck(string[] arr) 
+        static public bool CheckEmptyValues(string v1, string v2, string v3)
+        {
+            if (v1 != "_" && v2 != "_" && v3 != "_")
+                return true;
+            else
+                return false;
+        }
+
+        static public bool LeftVerticalCheck(string[] arr) 
         {
             string v1 = arr[0];
             string v2 = arr[3];
             string v3 = arr[6];
-            if(v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("LV Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
 
-        static public void RightVerticalCheck(string[] arr)
+        static public bool RightVerticalCheck(string[] arr)
         {
             string v1 = arr[2];
             string v2 = arr[5];
             string v3 = arr[8];
-            if (v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
             {
-                Console.WriteLine("RV Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
-        static public void MiddleVerticalCheck(string[] arr)
+        static public bool MiddleVerticalCheck(string[] arr)
         {
             string v1 = arr[1];
             string v2 = arr[4];
             string v3 = arr[7];
-            if (v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("MV Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
 
-        static public void TopHorizontalCheck(string[] arr)
+        static public bool TopHorizontalCheck(string[] arr)
         {
             string v1 = arr[0];
             string v2 = arr[1];
             string v3 = arr[2];
-            if (v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("TH Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
-        static public void MiddleHorizontalCheck(string[] arr)
+        static public bool MiddleHorizontalCheck(string[] arr)
         {
             string v1 = arr[3];
             string v2 = arr[4];
             string v3 = arr[5];
-            if (v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("MH Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
-        static public void BottomHorizontalCheck(string[] arr)
+        static public bool BottomHorizontalCheck(string[] arr)
         {
             string v1 = arr[6];
             string v2 = arr[7];
             string v3 = arr[8];
-            if (v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("BH Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
 
-        static public void LeftToRightDiagnol(string[] arr)
+        static public bool LeftToRightDiagnol(string[] arr)
         {
             string v1 = arr[0];
             string v2 = arr[4];
             string v3 = arr[8];
-            if (v1 == v2 && v2 == v3)
+            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("LRD Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
 
-        static public void RightToLeftDiagnol(string[] arr)
+        static public bool RightToLeftDiagnol(string[] arr)
         {
-            string v1 = arr[0];
+            string v1 = arr[2];
             string v2 = arr[4];
-            string v3 = arr[8];
-            if (v1 == v2 && v2 == v3)
+            string v3 = arr[6];
+            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
             {
-                Console.WriteLine("RLD Matched");
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
             }
+            else
+                return false;
         }
     }
 }
