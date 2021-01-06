21d6541b3e3cc896815f8110810158a70db2ad01
thalif <thalif12@gmail.com>
2021-01-06 23:48:28 +0530

    Game is Ready to Play withour Draw state

--- NewTicTacToe/NewTicTacToe/Program.cs
+++ NewTicTacToe/NewTicTacToe/Program.cs
@@ -21,17 +21,18 @@ namespace NewTicTacToe
 
         static public void LoadBoard()
         {
-            Console.Write("\n");
+            Console.Write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             for (int i = 0; i < PlayBoard.Length; i++)
             {
-                Console.Write(PlayBoard[i] + "\t");
+                Console.Write(PlayBoard[i] + "\t\t\t\t");
                 if (i == 2 || i == 5 || i == 8)
-                    Console.Write("\n\n");
+                    Console.Write("\n\n\n\n");
             }
         }
 
         static public int Player1Input()
         {
+            Console.Write("Player\tX\t|");
             string a = Console.ReadLine();
             int input = int.Parse(a);
 
@@ -49,6 +50,7 @@ namespace NewTicTacToe
         }
         static public int Player2Input()
         {
+            Console.Write("Player\tO\t|");
             string a = Console.ReadLine();
             int input = int.Parse(a);
 
@@ -67,6 +69,7 @@ namespace NewTicTacToe
 
         static public void GameStart()
         {
+            GameValidation gv = new GameValidation();
             for (int i = 0; i < 9; i++)
             {
                 if (Player1)
@@ -75,152 +78,12 @@ namespace NewTicTacToe
                     i -= Player2Input();
 
                 if (i >= 4)
-                    if (ValidationProcess(PlayBoard))
+                    
+                    if (gv.ValidationProcess(PlayBoard))
                         break;
             }
         }
 
-        static public bool ValidationProcess(string[] arr)
-        {
-            if (LeftVerticalCheck(arr))
-                return true;
-
-            if (RightVerticalCheck(arr))
-                return true;
-
-            if (MiddleVerticalCheck(arr))
-                return true;
-
-            if (TopHorizontalCheck(arr))
-                return true;
-            if (MiddleHorizontalCheck(arr))
-                return true;
-            if (BottomHorizontalCheck(arr))
-                return true;
-
-            if (LeftToRightDiagnol(arr))
-                return true;
-
-            if (RightToLeftDiagnol(arr))
-                return true;
-            return false;
-        }
-
-        static public bool CheckEmptyValues(string v1, string v2, string v3)
-        {
-            if (v1 != "_" && v2 != "_" && v3 != "_")
-                return true;
-            else
-                return false;
-        }
-
-        static public bool LeftVerticalCheck(string[] arr) 
-        {
-            string v1 = arr[0];
-            string v2 = arr[3];
-            string v3 = arr[6];
-            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-
-        static public bool RightVerticalCheck(string[] arr)
-        {
-            string v1 = arr[2];
-            string v2 = arr[5];
-            string v3 = arr[8];
-            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-        static public bool MiddleVerticalCheck(string[] arr)
-        {
-            string v1 = arr[1];
-            string v2 = arr[4];
-            string v3 = arr[7];
-            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-
-        static public bool TopHorizontalCheck(string[] arr)
-        {
-            string v1 = arr[0];
-            string v2 = arr[1];
-            string v3 = arr[2];
-            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-        static public bool MiddleHorizontalCheck(string[] arr)
-        {
-            string v1 = arr[3];
-            string v2 = arr[4];
-            string v3 = arr[5];
-            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-        static public bool BottomHorizontalCheck(string[] arr)
-        {
-            string v1 = arr[6];
-            string v2 = arr[7];
-            string v3 = arr[8];
-            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-
-        static public bool LeftToRightDiagnol(string[] arr)
-        {
-            string v1 = arr[0];
-            string v2 = arr[4];
-            string v3 = arr[8];
-            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
-
-        static public bool RightToLeftDiagnol(string[] arr)
-        {
-            string v1 = arr[2];
-            string v2 = arr[4];
-            string v3 = arr[6];
-            if (v1 == v2 && v2 == v3&& CheckEmptyValues(v1,v2,v3))
-            {
-                Console.WriteLine("Player\t" + v1 + "\tWon Game");
-                return true;
-            }
-            else
-                return false;
-        }
+        
     }
 }
